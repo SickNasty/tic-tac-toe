544a64529bbf43f386474da36c936219056c0e05
max <max.zorion@gmail.com>
2016-11-29 13:29:24 -0800

    my Tic tac toe

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,39 @@
 // Write all your JavaScript code in this file!
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
+function burritobuster(){
+  var circle = document.createElementNS(namespace,"circle")
+  circle.setAttribute("cx",175)
+  circle.setAttribute("cy",55)
+  circle.setAttribute("r",15)
+  circle.setAttribute("fill","blue")
+  var canvas = document.getElementById("game-board")
+  canvas.appendChild(circle)
+}
+  function soowoo(){
+    var circle = document.createElementNS(namespace,"circle")
+    circle.setAttribute("cx",115)
+    circle.setAttribute("cy",55)
+    circle.setAttribute("r",15)
+    circle.setAttribute("fill","blue")
+    var canvas = document.getElementById("game-board")
+    canvas.appendChild(circle)
+  }
+  function draxxlemsklounst(){
+    var circle = document.createElementNS(namespace,"circle")
+    circle.setAttribute("cx",235)
+    circle.setAttribute("cy",55)
+    circle.setAttribute("r",15)
+    circle.setAttribute("fill","blue")
+    var canvas = document.getElementById("game-board")
+    canvas.appendChild(circle)
+  }
+function hue(){
+  var circle = document.createElementNS(namespace,"circle")
+  circle.setAttribute("cx",115)
+  circle.setAttribute("cy",115)
+  circle.setAttribute("r",15)
+  circle.setAttribute("fill","blue")
+  var canvas = document.getElmentById("game-board")
+  canvas.appendChild(circle)
+}
